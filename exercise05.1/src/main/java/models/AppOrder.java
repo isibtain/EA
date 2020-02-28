3d04da6ced1ea723ac5760778ffa8167f50eeced
Sibtain Raza <47073093+isibtain@users.noreply.github.com>
2020-02-28 07:36:00 -0600

    AppOrder Name updated

--- exercise05.1/src/main/java/models/AppOrder.java
+++ exercise05.1/src/main/java/models/AppOrder.java
@@ -16,7 +16,7 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 
-public class AppStudent {
+public class AppOrder {
 
 
 	
